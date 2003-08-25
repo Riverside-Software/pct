baedf70c7dc55937ec62660735957d7bafd375d4
justus_phenix <justus_phenix@localhost>
2003-08-25 09:35:01 +0000

    Proxygen task

---
+++ src/java/com/phenix/pct/PCTProxygen.java
@@ -0,0 +1,132 @@
+/*
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2002 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "Ant" and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+package com.phenix.pct;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.taskdefs.Java;
+import org.apache.tools.ant.types.Environment;
+import org.apache.tools.ant.types.Path;
+
+import java.io.File;
+
+
+/**
+ * @author Gilles QUERRET
+ */
+public class PCTProxygen extends PCT {
+    private File srcFile = null;
+    private boolean keepFiles = false;
+
+    /**
+     * @param keepFiles
+     */
+    public void setKeepFiles(boolean keepFiles) {
+        this.keepFiles = keepFiles;
+    }
+
+    /**
+     * @param srcFile
+     */
+    public void setSrcFile(File srcFile) {
+        this.srcFile = srcFile;
+    }
+
+    public void execute() throws BuildException {
+        if (this.srcFile == null) {
+            throw new BuildException("Proxygen file not defined");
+        }
+
+        if (this.getDlcHome() == null) {
+            throw new BuildException("Progress installation directory not defined");
+        }
+
+        // Creates a new Java task to launch proxygen task
+        Java pxg = new Java();
+        pxg.setOwningTarget(this.getOwningTarget());
+        pxg.setTaskName(this.getTaskName());
+
+        Project project = this.getProject();
+        project.setBaseDir(this.getBaseDir());
+        pxg.setProject(project);
+        pxg.setDir(this.getBaseDir());
+        pxg.setClassname("com.progress.open4gl.proxygen.Batch");
+
+        // Add proxygen.zip to classpath
+        Path p = new Path(this.getProject());
+        p.setPath(this.getDlcHome().toString() + File.separatorChar + "java" + File.separatorChar +
+                  "proxygen.zip");
+        pxg.createClasspath();
+        pxg.setClasspath(p);
+
+        // As Progress doesn't know command line parameters, arguments are given via environment variables        
+        Environment.Variable var = new Environment.Variable();
+        var.setKey("PXGFile");
+        var.setValue(this.srcFile.toString());
+        pxg.addSysproperty(var);
+
+        Environment.Variable var2 = new Environment.Variable();
+        var2.setKey("Install.Dir");
+        var2.setValue(this.getDlcHome().toString());
+        pxg.addSysproperty(var2);
+
+        Environment.Variable var3 = new Environment.Variable();
+        var3.setKey("Proxygen.LeaveProxyFiles");
+        var3.setValue((this.keepFiles ? "true" : "false"));
+        pxg.addSysproperty(var3);
+
+        // Don't use executeJava and get return code as Progress doesn't know what a return value is
+        pxg.execute();
+    }
+}
