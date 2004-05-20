c2750f71d9561551e0ac2b10575593f7df9b7490
Gilles QUERRET <jakejustus@users.noreply.github.com>
2004-05-20 07:44:08 +0000

    Header cleaning

--- src/test/com/phenix/pct/PCTCompileTest.java
+++ src/test/com/phenix/pct/PCTCompileTest.java
@@ -1,42 +1,55 @@
 /*
  * The Apache Software License, Version 1.1
  *
- * Copyright (c) 2002 The Apache Software Foundation. All rights reserved.
+ * Copyright (c) 2002 The Apache Software Foundation.  All rights
+ * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *  1. Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- *  2. Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution.
- *  3. The end-user documentation included with the redistribution, if any,
- * must include the following acknowlegement: "This product includes software
- * developed by the Apache Software Foundation (http://www.apache.org/)."
- * Alternately, this acknowlegement may appear in the software itself, if and
- * wherever such third-party acknowlegements normally appear.
- *  4. The names "Ant" and "Apache Software Foundation" must not be used to
- * endorse or promote products derived from this software without prior written
- * permission. For written permission, please contact apache@apache.org.
- *  5. Products derived from this software may not be called "Apache" nor may
- * "Apache" appear in their names without prior written permission of the
- * Apache Group.
+ * modification, are permitted provided that the following conditions
+ * are met:
  *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * APACHE SOFTWARE FOUNDATION OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "Ant" and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  * ====================================================================
  *
- * This software consists of voluntary contributions made by many individuals
- * on behalf of the Apache Software Foundation. For more information on the
- * Apache Software Foundation, please see <http://www.apache.org/> .
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
  */
 package com.phenix.pct;
 
@@ -172,6 +185,7 @@ public class PCTCompileTest extends BuildFileTest {
     public void test11() {
         executeTarget("test11init");
         expectBuildException("test11", "Second task should not be launched");
+
         File f = new File("src/test/build/sandbox/temp2.r");
         assertFalse(f.exists());
     }
@@ -196,24 +210,28 @@ public class PCTCompileTest extends BuildFileTest {
     }
 
     public void test14() {
-    	executeTarget("test14init");
-    	executeTarget("test14");
+        executeTarget("test14init");
+        executeTarget("test14");
+
         File f = new File("src/test/build/sandbox/test.r");
-        File f2 = new File("src/test/build/sandbox/test2.r"); 
+        File f2 = new File("src/test/build/sandbox/test2.r");
         assertTrue(f.exists());
         assertTrue(f2.exists());
+
         long mod = f.lastModified();
         long mod2 = f2.lastModified();
         executeTarget("test14bis");
         assertTrue(mod < f.lastModified());
         assertTrue(mod2 < f2.lastModified());
     }
-    
+
     public void test15() {
-    	executeTarget("test15init");
-    	executeTarget("test15");
+        executeTarget("test15init");
+        executeTarget("test15");
+
         File f = new File("src/test/build/sandbox/test.r");
         assertTrue(f.exists());
+
         long mod = f.lastModified();
         executeTarget("test15bis");
         assertTrue(mod == f.lastModified());
