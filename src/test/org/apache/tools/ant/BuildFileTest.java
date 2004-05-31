34a6263e2376b9ef66d2580289e675116fd8a970
Gilles QUERRET <jakejustus@users.noreply.github.com>
2004-05-31 10:58:45 +0000

    Merged from ANT cvs tree

--- src/test/org/apache/tools/ant/BuildFileTest.java
+++ src/test/org/apache/tools/ant/BuildFileTest.java
@@ -1,55 +1,18 @@
 /*
- * The Apache Software License, Version 1.1
+ * Copyright  2001-2004 The Apache Software Foundation
  *
- * Copyright (c) 2001-2003 The Apache Software Foundation.  All rights
- * reserved.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The end-user documentation included with the redistribution, if
- *    any, must include the following acknowlegement:
- *       "This product includes software developed by the
- *        Apache Software Foundation (http://www.apache.org/)."
- *    Alternately, this acknowlegement may appear in the software itself,
- *    if and wherever such third-party acknowlegements normally appear.
- *
- * 4. The names "Ant" and "Apache Software
- *    Foundation" must not be used to endorse or promote products derived
- *    from this software without prior written permission. For written
- *    permission, please contact apache@apache.org.
- *
- * 5. Products derived from this software may not be called "Apache"
- *    nor may "Apache" appear in their names without prior written
- *    permission of the Apache Group.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the Apache Software Foundation.  For more
- * information on the Apache Software Foundation, please see
- * <http://www.apache.org/>.
  */
 
 package org.apache.tools.ant;
@@ -67,8 +30,6 @@ import java.net.URL;
  * This class provides a number of utility methods for particular build file
  * tests which extend this class.
  *
- * @author Nico Seessle <nico@seessle.de>
- * @author Conor MacNeill
  */
 public abstract class BuildFileTest extends TestCase {
 
@@ -108,7 +69,7 @@ public abstract class BuildFileTest extends TestCase {
         String realLog = getLog();
         assertEquals(log, realLog);
     }
-    
+
     /**
      * Assert that the given substring is in the log messages
      */
@@ -119,7 +80,7 @@ public abstract class BuildFileTest extends TestCase {
                    + realLog + "\"",
                    realLog.indexOf(substring) >= 0);
     }
-    
+
     /**
      * Assert that the given message has been logged with a priority
      * &gt;= INFO when running the given target.
@@ -215,11 +176,7 @@ public abstract class BuildFileTest extends TestCase {
             if (!cr) {
                 cleanedBuffer.append(ch);
             } else {
-                if (ch == '\n') {
-                    cleanedBuffer.append(ch);
-                } else {
-                    cleanedBuffer.append('\r').append(ch);
-                }
+                cleanedBuffer.append(ch);
             }
         }
         return cleanedBuffer.toString();
@@ -241,13 +198,13 @@ public abstract class BuildFileTest extends TestCase {
      */
     protected void configureProject(String filename, int logLevel)
         throws BuildException {
-		logBuffer = new StringBuffer();
+        logBuffer = new StringBuffer();
         fullLogBuffer = new StringBuffer();
         project = new Project();
         project.init();
         project.setUserProperty( "ant.file" , new File(filename).getAbsolutePath() );
         project.addBuildListener(new AntTestListener(logLevel));
-		ProjectHelper.configureProject(project, new File(filename));
+        ProjectHelper.configureProject(project, new File(filename));
     }
 
     /**
@@ -418,12 +375,12 @@ public abstract class BuildFileTest extends TestCase {
      */
     private static class AntOutputStream extends java.io.OutputStream {
         private StringBuffer buffer;
-        
+
         public AntOutputStream( StringBuffer buffer ) {
             this.buffer = buffer;
         }
-        
-        public void write(int b) { 
+
+        public void write(int b) {
             buffer.append((char)b);
         }
     }
@@ -519,4 +476,3 @@ public abstract class BuildFileTest extends TestCase {
 
 
 }
-
