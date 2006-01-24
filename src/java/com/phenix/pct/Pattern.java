8aa1befdc4dce6f6a89e3e7a272db4d2f79ae633
Gilles QUERRET <jakejustus@users.noreply.github.com>
2006-01-24 15:57:09 +0000

    PCTBinaryDump implementation

---
+++ src/java/com/phenix/pct/Pattern.java
@@ -0,0 +1,13 @@
+package com.phenix.pct;
+
+public class Pattern {
+    private String name = null;
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+}
