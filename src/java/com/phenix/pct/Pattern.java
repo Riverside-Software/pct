7333478ead260edbcdaa375ade016c73a6c44be5
Gilles QUERRET <jakejustus@users.noreply.github.com>
2006-03-21 17:00:12 +0000

    Bug #1455512 : corrected bug on include/exclude patterns

--- src/java/com/phenix/pct/Pattern.java
+++ src/java/com/phenix/pct/Pattern.java
@@ -2,6 +2,7 @@ package com.phenix.pct;
 
 public class Pattern {
     private String name = null;
+    private boolean include = true;
     
     public void setName(String name) {
         this.name = name;
@@ -9,5 +10,14 @@ public class Pattern {
     
     public String getName() {
         return this.name;
+    }
+    
+    // Use this method to declare an Include node
+    public void setInclude(boolean include) {
+        this.include = include;
+    }
+    
+    public boolean isInclude() {
+        return this.include;
     }
 }
