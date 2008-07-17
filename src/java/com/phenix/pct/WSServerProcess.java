3b3d0d2222c05db953e55182a55283a7772293fe
justus_phenix <justus_phenix@localhost>
2008-07-17 15:01:53 +0000

    Removed extra separator when building PROPATH property and corrected constructor [Luis ROMERO]

--- src/java/com/phenix/pct/WSServerProcess.java
+++ src/java/com/phenix/pct/WSServerProcess.java
@@ -7,20 +7,24 @@ import org.apache.tools.ant.BuildException;
 
 public class WSServerProcess extends PCTRun {
     private boolean webLogError = true;
-    
+
     public WSServerProcess() {
+        super(false, false);
         this.procedure = "web\\objects\\web-disp.p";
     }
-    
+
     public void setGraphicalMode(boolean graphMode) {
         throw new BuildException("Unknown attribute");
     }
+
     public void setDebugPCT(boolean debugPCT) {
         throw new BuildException("Unknown attribute");
     }
+
     public void setBaseDir(File baseDir) {
         throw new BuildException("Unknown attribute");
     }
+
     public void setFailOnError(boolean failOnError) {
         throw new BuildException("Unknown attribute");
     }
@@ -32,13 +36,29 @@ public class WSServerProcess extends PCTRun {
     public void setWebLogError(boolean webLogError) {
         this.webLogError = webLogError;
     }
-    
+
     protected List getCmdLineParameters() {
         List list = super.getCmdLineParameters();
-        
+
         if (this.webLogError)
             list.add("-weblogerror");
-        
+
         return list;
+    }
+
+    /**
+     * Get the current propath as a path-separated list
+     * 
+     * @return String
+     */
+    public String getPropath() {
+        StringBuffer propathList = new StringBuffer("");
+        String[] lst = this.propath.list();
+        for (int k = 0; k < lst.length; k++) {
+            propathList.append(lst[k]);
+            if (k < lst.length - 1)
+                propathList.append(File.pathSeparatorChar);
+        }
+        return propathList.toString();
     }
 }
