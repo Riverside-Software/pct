0917b937f9cec4728bb04adc7b75173d36ae16fb
Gilles QUERRET <jakejustus@users.noreply.github.com>
2008-07-17 14:51:19 +0000

    Removed extra separator when building PROPATH property [Luis ROMERO]

--- src/java/com/phenix/pct/ServerProcess.java
+++ src/java/com/phenix/pct/ServerProcess.java
@@ -11,7 +11,7 @@ public class ServerProcess extends PCTRun {
     private String disconnectProc = null;
     private String startupProc = null;
     private String shutdownProc = null;
-    
+
     public ServerProcess() {
         super(false, false);
     }
@@ -19,56 +19,70 @@ public class ServerProcess extends PCTRun {
     public void setActivateProc(String activateProc) {
         this.activateProc = activateProc;
     }
+
     public void setConnectProc(String connectProc) {
         this.connectProc = connectProc;
     }
+
     public void setDeactivateProc(String deactivateProc) {
         this.deactivateProc = deactivateProc;
     }
+
     public void setDisconnectProc(String disconnectProc) {
         this.disconnectProc = disconnectProc;
     }
+
     public void setShutdownProc(String shutdownProc) {
         this.shutdownProc = shutdownProc;
     }
     public void setStartupProc(String startupProc) {
         this.startupProc = startupProc;
     }
+
     public void setProcedure(String procedure) {
         throw new BuildException("Unknown attribute");
     }
+
     public void setGraphicalMode(boolean graphMode) {
         throw new BuildException("Unknown attribute");
     }
+
     public void setDebugPCT(boolean debugPCT) {
         throw new BuildException("Unknown attribute");
     }
+
     public void setBaseDir(File baseDir) {
         throw new BuildException("Unknown attribute");
     }
+
     public void setFailOnError(boolean failOnError) {
         throw new BuildException("Unknown attribute");
     }
-    
+
     public String getActivateProc() {
         return activateProc;
     }
+
     public String getConnectProc() {
         return connectProc;
     }
+
     public String getDeactivateProc() {
         return deactivateProc;
     }
+
     public String getDisconnectProc() {
         return disconnectProc;
     }
+
     public String getShutdownProc() {
         return shutdownProc;
     }
+
     public String getStartupProc() {
         return startupProc;
     }
-    
+
     /**
      * Get the current propath as a path-separated list
      * 
@@ -77,9 +91,12 @@ public class ServerProcess extends PCTRun {
     public String getPropath() {
         StringBuffer propathList = new StringBuffer("");
         String[] lst = this.propath.list();
-        for (int k = 0; k < lst.length ; k++) {
-            propathList.append(lst[k] + File.pathSeparatorChar);
-        }                   
+        for (int k = 0; k < lst.length; k++) {
+            propathList.append(lst[k]);
+            if (k < lst.length - 1)
+                propathList.append(File.pathSeparatorChar);
+
+        }
         return propathList.toString();
     }
 }
