b76009fb048b5b168d6a66ad2f27411e10858223
Gilles QUERRET <jakejustus@users.noreply.github.com>
2011-01-26 13:26:21 +0100

    Added PCTVersion task

---
+++ src/java/com/phenix/pct/Version.java
@@ -0,0 +1,23 @@
+package com.phenix.pct;
+
+import java.util.ResourceBundle;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+import org.apache.tools.ant.taskdefs.Echo;
+
+public class Version extends Task {
+    private static final String BUNDLE_NAME = "com.phenix.pct.PCT"; //$NON-NLS-1$
+
+    private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+
+    @Override
+    public void execute() throws BuildException {
+        String str = "PCT Version " + RESOURCE_BUNDLE.getString("PCTVersion") + " - Build "
+                + RESOURCE_BUNDLE.getString("PCTBuild");
+        Echo echo = new Echo();
+        echo.bindToOwner(this);
+        echo.setMessage(str);
+        echo.execute();
+    }
+}
