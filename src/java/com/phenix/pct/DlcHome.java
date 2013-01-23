0cec28dcde7d23210531fd7376d6c63b4a9d457d
Gilles QUERRET <jakejustus@users.noreply.github.com>
2013-01-23 20:14:12 +0100

    Added /.pct/** to DefaultExcludes

--- src/java/com/phenix/pct/DlcHome.java
+++ src/java/com/phenix/pct/DlcHome.java
@@ -4,6 +4,7 @@ import java.io.File;
 
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
+import org.apache.tools.ant.taskdefs.DefaultExcludes;
 
 public class DlcHome extends Task {
     private File value = null;
@@ -12,14 +13,19 @@ public class DlcHome extends Task {
     public void execute() throws BuildException {
         if (value == null)
             throw new BuildException("Value can't be null");
-
         dlcHome = value;
+
+        DefaultExcludes excludes = new DefaultExcludes();
+        excludes.bindToOwner(this);
+        excludes.setAdd("/.pct/**");
+        excludes.execute();
     }
 
     public void setValue(File file) {
         this.value = file;
     }
 
+    // Not really good in multi-threaded case...
     private static File dlcHome = null;
     public static File getDlcHome() {
         return dlcHome;
