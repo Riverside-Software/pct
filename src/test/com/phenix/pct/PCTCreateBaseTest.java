06562adbc17236e689e6e157960922a57179e41b
Gilles QUERRET <g.querret@riverside-software.fr>
2014-02-15 12:46:32 +0100

    Enterprise lic available only on Linux

--- src/test/com/phenix/pct/PCTCreateBaseTest.java
+++ src/test/com/phenix/pct/PCTCreateBaseTest.java
@@ -201,7 +201,7 @@ public class PCTCreateBaseTest extends BuildFileTestNg {
         executeTarget("verify");
     }
 
-    @Test(groups = {"v10"})
+    @Test(groups = {"v10", "unix"})
     public void test14() {
         configureProject("PCTCreateBase/test14/build.xml");
         
