2399b13cb4886488602590f7cc1451c6469adcb7
Bastien THORAL <b.thoral@riverside-software.fr>
2014-06-17 17:32:13 +0200

    Adding unit tests for OEunit task.

--- src/test/com/phenix/pct/OEUnitTest.java
+++ src/test/com/phenix/pct/OEUnitTest.java
@@ -53,6 +53,10 @@
  */
 package com.phenix.pct;
 
+import static org.testng.Assert.assertTrue;
+
+import java.io.File;
+
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
@@ -70,21 +74,64 @@ public class OEUnitTest extends BuildFileTestNg {
     private final XPath xpath = XPathFactory.newInstance().newXPath();
 
     // Regular run of 2 test procedures
-    @Test(groups = {"v10,v11"})
+    @Test(groups = {"v10"})
     public void test1() throws XPathExpressionException {
         configureProject("OEUnit/test1/build.xml");
         executeTarget("test");
 
-        InputSource inputSource = new InputSource("ABLUnit/test1/test-reports/FirstOEUnitTest.xml");
-        // Should be 2/2/2
+        InputSource inputSource = new InputSource("OEUnit/test1/OEReports/FirstOEUnitTest.xml");
         Assert.assertEquals(xpath.evaluate("/testsuite/@tests", inputSource), "3");
         Assert.assertEquals(xpath.evaluate("/testsuite/@failures", inputSource), "0");
         Assert.assertEquals(xpath.evaluate("/testsuite/@errors", inputSource), "0");
-        
-        inputSource = new InputSource("ABLUnit/test1/test-reports/FirstOEUnitTest.xml");
-        // Should be 2/2/2
+
+        inputSource = new InputSource("OEUnit/test1/OEReports/StandAloneTests.xml");
         Assert.assertEquals(xpath.evaluate("/testsuite/@tests", inputSource), "1");
         Assert.assertEquals(xpath.evaluate("/testsuite/@failures", inputSource), "0");
         Assert.assertEquals(xpath.evaluate("/testsuite/@errors", inputSource), "0");
+    }
+
+    // No output dir, default value instead
+    @Test(groups = {"v10"})
+    public void test2() throws XPathExpressionException {
+        configureProject("OEUnit/test2/build.xml");
+        executeTarget("test");
+
+        InputSource inputSource = new InputSource("OEUnit/test2/FirstOEUnitTest.xml");
+        Assert.assertEquals(xpath.evaluate("/testsuite/@tests", inputSource), "3");
+        Assert.assertEquals(xpath.evaluate("/testsuite/@failures", inputSource), "0");
+        Assert.assertEquals(xpath.evaluate("/testsuite/@errors", inputSource), "0");
+
+        inputSource = new InputSource("OEUnit/test2/StandAloneTests.xml");
+        Assert.assertEquals(xpath.evaluate("/testsuite/@tests", inputSource), "1");
+        Assert.assertEquals(xpath.evaluate("/testsuite/@failures", inputSource), "0");
+        Assert.assertEquals(xpath.evaluate("/testsuite/@errors", inputSource), "0");
+    }
+
+    // Running a test suite class
+    @Test(groups = {"v10"})
+    public void test3() throws XPathExpressionException {
+        configureProject("OEUnit/test3/build.xml");
+        executeTarget("test");
+
+        InputSource inputSource = new InputSource("OEUnit/test3/FirstTestSuite.xml");
+        Assert.assertEquals(xpath.evaluate("/testsuites/@tests", inputSource), "4");
+        Assert.assertEquals(xpath.evaluate("/testsuites/@failures", inputSource), "0");
+        Assert.assertEquals(xpath.evaluate("/testsuites/@errors", inputSource), "0");
+    }
+
+    // Test all report formats
+    @Test(groups = {"v10"})
+    public void test4() throws XPathExpressionException {
+        configureProject("OEUnit/test4/build.xml");
+        executeTarget("test");
+
+        File f = new File("OEUnit/test4/FirstOEUnitTest.csv");
+        assertTrue(f.exists());
+        f = new File("OEUnit/test4/FirstOEUnitTest.txt");
+        assertTrue(f.exists());
+        f = new File("OEUnit/test4/FirstOEUnitTest.xml");
+        assertTrue(f.exists());
+        f = new File("OEUnit/test4/SureFire-report/FirstOEUnitTest.xml");
+        assertTrue(f.exists());
     }
 }
