7d11e3fd22bfff18149f290ef8b0248921026c5b
Gilles QUERRET <jakejustus@users.noreply.github.com>
2014-08-14 11:29:50 +0200

    Class documentation templates update (by Consultingwerk)

--- src/progress/Consultingwerk/Studio/ClassDocumentation/Templates/resources/functions.js
+++ src/progress/Consultingwerk/Studio/ClassDocumentation/Templates/resources/functions.js
@@ -1,14 +1,3 @@
-/* *********************************************************************
- * Copyright (C) 2006-2013 by Consultingwerk Ltd. ("CW") -            *
- * www.consultingwerk.de and other contributors as listed             *
- * below.  All Rights Reserved.                                       *
- *                                                                    *
- *  Software is distributed on an "AS IS", WITHOUT WARRANTY OF ANY    *
- *   KIND, either express or implied.                                 *
- *                                                                    *
- *  Contributors:                                                     *
- *                                                                    *
- ********************************************************************* */
 /*------------------------------------------------------------------------
     File        : functions.js
     Purpose     : Implements javascript functionality for the
