2ca951efb1f799abc4a47c2c7f36255cd728de1a
Gilles QUERRET <g.querret@riverside-software.fr>
2019-06-14 13:19:40 +0200

    Copyright

--- src/java/eu/rssw/rcode/Destructor.java
+++ src/java/eu/rssw/rcode/Destructor.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2011-2018 Riverside Software
+ * Copyright 2011-2019 Riverside Software
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
--- src/java/za/co/mip/ablduck/ABLDuckClassVisitor.java
+++ src/java/za/co/mip/ablduck/ABLDuckClassVisitor.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2017-2018 MIP Holdings
+ * Copyright 2017-2019 MIP Holdings
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
