79a04c578ba4141cdb90573101e2a7f2481c2f6e
Gilles QUERRET <g.querret@riverside-software.fr>
2019-06-28 09:48:38 +0000

    Instructions for Maven Central

--- mvncentral.sh
+++ mvncentral.sh
@@ -8,6 +8,8 @@
 #        <gpg.passphrase>INSERT VALUE HERE</gpg.passphrase>
 #      </properties>
 #    </profile>
+# 3/ When script is executed, go to https://oss.sonatype.org/#stagingRepositories, then close and release the staging repository
+# 4/ It then takes a few minutes before artifacts are visible in Maven Central
 
 mvn org.apache.maven.plugins:maven-gpg-plugin:1.3:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2 -DrepositoryId=ossrh -DpomFile=pom.xml -Dfile=dist/PCT.jar -Pgpg
 mvn org.apache.maven.plugins:maven-gpg-plugin:1.3:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2 -DrepositoryId=ossrh -DpomFile=pom.xml -Dfile=dist/PCT-sources.jar -Dclassifier=sources -Pgpg
