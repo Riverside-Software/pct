a0f57f4320c8b52ce4e541bd9050f0d09658e38a
Gilles QUERRET <g.querret@riverside-software.fr>
2020-02-07 09:23:39 +0000

    Maven Central script

--- mvncentral.sh
+++ mvncentral.sh
@@ -11,6 +11,6 @@
 # 3/ When script is executed, go to https://oss.sonatype.org/#stagingRepositories, then close and release the staging repository
 # 4/ It then takes a few minutes before artifacts are visible in Maven Central
 
-mvn org.apache.maven.plugins:maven-gpg-plugin:1.3:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2 -DrepositoryId=ossrh -DpomFile=pom.xml -Dfile=dist/PCT.jar -Pgpg
-mvn org.apache.maven.plugins:maven-gpg-plugin:1.3:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2 -DrepositoryId=ossrh -DpomFile=pom.xml -Dfile=dist/PCT-sources.jar -Dclassifier=sources -Pgpg
-mvn org.apache.maven.plugins:maven-gpg-plugin:1.3:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2 -DrepositoryId=ossrh -DpomFile=pom.xml -Dfile=dist/PCT-javadoc.jar -Dclassifier=javadoc -Pgpg
+mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2 -DrepositoryId=ossrh -DpomFile=pom.xml -Dfile=dist/PCT.jar -Pgpg
+mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2 -DrepositoryId=ossrh -DpomFile=pom.xml -Dfile=dist/PCT-sources.jar -Dclassifier=sources -Pgpg
+mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2 -DrepositoryId=ossrh -DpomFile=pom.xml -Dfile=dist/PCT-javadoc.jar -Dclassifier=javadoc -Pgpg
