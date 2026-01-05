02742a1a6d5ae4c544a6478f9509ee61d6c30670
Gilles Querret <g.querret@riverside-software.fr>
2026-01-05 15:51:45 +0100

    Switch to released version of mvn-resolver

---
+++ src/java/eu/rssw/openedge/ls/mapping/ProjectConfigFile.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2005-2025 Riverside Software
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package eu.rssw.openedge.ls.mapping;
+
+import com.google.gson.annotations.SerializedName;
+
+// Simplified JSON mapping of project config file, copied from language server module
+public class ProjectConfigFile {
+
+    @SerializedName(value = "dependencies")
+    public Dependency[] dependencies;
+
+    public static class Dependency {
+        @SerializedName(value = "groupId")
+        public String groupId;
+        @SerializedName(value = "artifactId")
+        public String artifactId;
+        @SerializedName(value = "version")
+        public String version;
+        @SerializedName(value = "classifier")
+        public String classifier;
+        @SerializedName(value = "extension")
+        public String extension;
+    }
+
+}
